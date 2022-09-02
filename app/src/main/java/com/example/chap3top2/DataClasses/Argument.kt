eb8def5d42dcb9259fbef6ae6d038c447c3ce3d9
lusiro <lusiro2001@students.amikom.ac.id>
2022-09-02 19:36:34 +0700

    add class argument for parcelable data

--- app/src/main/java/com/example/chap3top2/DataClasses/Argument.kt
+++ app/src/main/java/com/example/chap3top2/DataClasses/Argument.kt
@@ -4,7 +4,7 @@ import android.os.Parcel
 import android.os.Parcelable
 
 data class Argument(val argumentSatu: String?, val argumentDua: String?): Parcelable {
-    
+
     constructor(parcel: Parcel) : this(
         parcel.readString(),
         parcel.readString()
